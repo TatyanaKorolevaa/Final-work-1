0c9a064137fbd2339997f32477bb2461b183d4f9
TatyanaKorolevaa <[200~t310kor233@mail.ru~>
2023-12-18 22:21:15 +0300

    Program

---
+++ Program/Program.cs
@@ -0,0 +1,33 @@
+﻿// Примеры:
+// ["Hello", "2", "world", ":-)"] → [“2”, “:-)”]
+// ["1234", "1567", "-2", "computer science"] → [“-2”]
+// ["Russia", "Denmark", "Kazan"] → []
+
+string[] array = {"Hello", "2", "world", ":-)"};
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array [i].Length < 4)
+    {
+        count++;
+    }
+}
+
+string[] result = new string[count];
+
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array [i].Length < 4)
+    {
+        result[j]=array[i];
+        j++;
+    }
+}
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.Write($"{result[i]}\t");
+}
+
+
